ca27d876b024e605c1b44001eb9000526f171fac
edgardfn <edgardfinotti@yahoo.com.br>
2020-12-17 22:45:39 -0300

    Alterado imagem e informações do card Dados Pessoais.

--- quinzena5/perfilProfissional/labedin/labedin/src/App.js
+++ quinzena5/perfilProfissional/labedin/labedin/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import CardGrande from './components/CardGrande/CardGrande';
 import ImagemButton from './components/ImagemButton/ImagemButton';
+import imagemPerfil  from './imagem/foto.jpg'
 
 function App() {
   return (
@@ -9,9 +10,9 @@ function App() {
       <div className="page-section-container">
         <h2>Dados pessoais</h2>
         <CardGrande 
-          imagem="https://uploads-ssl.webflow.com/5d640f4558306be99cf47a0e/5dd57846babb597b77c6bb1d_PerfilFuture4_cor.png" 
-          nome="Astrodev" 
-          descricao="Oi, eu sou o Astrodev. Sou o chefe dos alunos da Labenu. Adoro pedir e-mails na sexta-feira e esperar os alunos responderem só para responder com uma bronca e dar mais trabalho para eles."
+          imagem={imagemPerfil}
+          nome="Edgard Finotti Neto" 
+          descricao="Sou apaixonado por tecnologia e principalmente programação, durante minha jornada profissional acabei me afastando da área de tecnologia, porém esse ano estou decidido a voltar para área e trabalhar com programação, atualmente estou realizando alguns cursos para me aperfeiçoar e conseguir uma oportunidade nessa área."
         />
         
         <ImagemButton 
