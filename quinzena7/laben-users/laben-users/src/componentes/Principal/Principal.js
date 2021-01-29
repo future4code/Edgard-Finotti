4d1105af26003024a50b1873ec612a8191907712
edgardfn <edgardfinotti@yahoo.com.br>
2021-01-28 22:14:21 -0300

    Alterado componente App.js para renderizar componte criado Principal.js

---
+++ quinzena7/laben-users/laben-users/src/componentes/Principal/Principal.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+import axios from 'axios'
+
+class Principal extends React.Component {
+    state = {
+
+    }
+
+    render () {
+
+        return (
+            <p>Página Principal</p>
+        )
+            
+        
+    }
+}
+
+export default Principal
