28533f5b1c930f85b839d09b9092c48721078954
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-02 14:56:46 -0300

    Criado arquivo requisicoes.js e contantes BASE_URL e axiosConfig para utilizar nas funções de interação com a Api.

--- quinzena7/laben-users/laben-users/src/componentes/Principal/Principal.js
+++ quinzena7/laben-users/laben-users/src/componentes/Principal/Principal.js
@@ -4,12 +4,11 @@ import axios from 'axios'
 import CadastroUsuario from '../CadastroUsuario/CadastroUsuario'
 import ListaUsuarios from '../ListaUsuarios/ListaUsuarios'
 import imagemExcluir from '../../images/remove.png'
+import {BASE_URL, axiosConfig} from '../../contants/requisicoes'
 
 const BotaoIrParaLista = styled.button`
     margin: 16px 0 64px 16px;
 `
-
-
 
 class Principal extends React.Component {
     state = {
@@ -43,36 +42,21 @@ class Principal extends React.Component {
         
         const body = novoUsuario
 
-        axios.post("https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-        body,
-        {
-            headers: {
-                Authorization:"edgard-finotti-muyembe"
-            }
-        }
-        )
-        .then(() => {
-            alert(`Usuário ${this.state.novoUsuario.name} adicionado com sucesso.`)
-            this.setState({cadastrar: false})
-            this.pegarListaUsuarios()
-        })
-        .catch((error) => alert(error))
+        axios.post(`${BASE_URL}/users`, body, axiosConfig)
+            .then(() => {
+                alert(`Usuário ${this.state.novoUsuario.name} adicionado com sucesso.`)
+                this.setState({cadastrar: false})
+                this.pegarListaUsuarios()
+            })
+            .catch((error) => alert(error))
     }
 
     pegarListaUsuarios = async () => {
 
         try {
             const resposta = await axios
-            .get(
-                "https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users",
-                {
-                    headers: {
-                        Authorization: "edgard-finotti-muyembe"
-                    }
-                }
-            )
+            .get(`${BASE_URL}/users`, axiosConfig)
             this.setState({ usuarios: resposta.data})
-
         } catch (erro) {
             alert(erro)
         }
@@ -80,13 +64,7 @@ class Principal extends React.Component {
     }
 
     excluirUsuarioLista = (usuario) => {
-        axios.delete(`https://us-central1-labenu-apis.cloudfunctions.net/labenusers/users/${usuario.id}`,
-            {
-                headers: {
-                    Authorization: "edgard-finotti-muyembe"
-                }
-            } 
-        )
+        axios.delete(`${BASE_URL}/users/${usuario.id}`, axiosConfig)
         .then(res => {
             alert(`Usuário ${usuario.name} excluido com sucesso !`)
             this.pegarListaUsuarios()
