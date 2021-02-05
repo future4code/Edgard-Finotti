5ddbf92b419264c4afa61240f8f42bfcc990ad00
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-04 21:32:56 -0300

    Adicionado menu no Cabacalho e estilizado.

--- quinzena7/spacex-Api/spacex-api/src/components/Cabecalho/Cabecalho.js
+++ quinzena7/spacex-Api/spacex-api/src/components/Cabecalho/Cabecalho.js
@@ -1,9 +1,20 @@
 import React from 'react'
-import { DivisaoCabecalho, NomeEmpresa, ImagemLogo } from './styles'
+import { DivisaoCabecalho, NomeEmpresa, ImagemLogo, DivisaoLogoENome, DivisaoMenu, ItemMenu, MenuLista } from './styles'
 
 export const Cabecalho = (props) => {
     return <DivisaoCabecalho>
-        <ImagemLogo src={props.imagemLogo} />
-        <NomeEmpresa>{props.nomeEmpresa}</NomeEmpresa>
+        <DivisaoLogoENome>
+            <ImagemLogo src={props.imagemLogo} />
+            <NomeEmpresa>{props.nomeEmpresa}</NomeEmpresa>
+        </DivisaoLogoENome>
+        
+        <DivisaoMenu>
+            <MenuLista>
+                <ItemMenu>Cápsulas</ItemMenu>
+                <ItemMenu>Tripulação</ItemMenu>
+                <ItemMenu>Lançamentos</ItemMenu>
+            </MenuLista>
+
+        </DivisaoMenu>
     </DivisaoCabecalho>
 }
--- quinzena7/spacex-Api/spacex-api/src/components/Cabecalho/styles.js
+++ quinzena7/spacex-Api/spacex-api/src/components/Cabecalho/styles.js
@@ -1,11 +1,20 @@
 import styled from 'styled-components'
 
 export const DivisaoCabecalho = styled.div`
+    /* display: flex;
+    align-items: center;
+    justify-content: center; */
+    display: flex;
+    justify-content: space-around;
+
+    height: 100%;
+`
+export const DivisaoLogoENome = styled.div`
+    height: 100%;
+
     display: flex;
     align-items: center;
     justify-content: center;
-
-    height: 100%;
 `
 
 export const NomeEmpresa = styled.h1`
@@ -15,3 +24,21 @@ export const NomeEmpresa = styled.h1`
 export const ImagemLogo = styled.img`
     height: 60%;
 `
+
+export const DivisaoMenu = styled.div`
+    height: 100%;
+
+    display: flex;
+    align-items: center;
+`
+export const MenuLista = styled.ul`
+    list-style: none;
+
+    display: flex;
+`
+export const ItemMenu = styled.li`
+    padding: 16px;
+    color: white;
+    cursor: pointer;
+`
+
