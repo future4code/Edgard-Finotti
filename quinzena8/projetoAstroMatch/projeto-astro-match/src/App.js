e06531e0b20ada9ce6c18d3c67a807e7c137990a
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-25 22:31:35 -0300

    Usado componente Css Baseline do Material para resetar estilos da página.

--- quinzena8/projetoAstroMatch/projeto-astro-match/src/App.js
+++ quinzena8/projetoAstroMatch/projeto-astro-match/src/App.js
@@ -1,12 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import { TelaPrincipal } from './screens/TelaPrincipal/TelaPrincipal';
+import CssBaseline from '@material-ui/core/CssBaseline'
 
 function App() {
   document.title = "AstroMatch"
-  
+
   return (
     <div className="App">
+      <CssBaseline />
       <TelaPrincipal />
     </div>
   );
