314e761de60cb9c509ef62ea37013a48a9d67605
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-28 11:11:46 -0300

    Criado componente Perfil que vai mostrar um perfil ao usuário,e criado estado para guardar este perfil que está sendo exibido.

--- quinzena8/projetoAstroMatch/projeto-astro-match/src/screens/TelaPrincipal/TelaPrincipal.js
+++ quinzena8/projetoAstroMatch/projeto-astro-match/src/screens/TelaPrincipal/TelaPrincipal.js
@@ -2,9 +2,11 @@ import React, {useState, useEffect} from 'react'
 import { Cabecalho } from '../../components/Cabecalho/Cabecalho'
 import { DivisaoPrincipal } from './stylesTelaPrincipal'
 import logo from '../../assets/logoAstroMatch.png'
+import { Perfil } from '../../components/Perfil/Perfil'
 
 export function TelaPrincipal() {
     const [ mostrarTelaInicial, setMostrarTelaInicial ] = useState(true)
+    const [ perfil, setPerfil ] = useState("")
     
     const mudarEstadoMostrarTelaInicial = () => {
         setMostrarTelaInicial( !mostrarTelaInicial )
@@ -19,8 +21,8 @@ export function TelaPrincipal() {
                 aoClicarSobreIcone= {mudarEstadoMostrarTelaInicial}
             />
 
-
-            TELA PRINCIPAL
+            <Perfil />
+            
         </DivisaoPrincipal>
     )
 }
