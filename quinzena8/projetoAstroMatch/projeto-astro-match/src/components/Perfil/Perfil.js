1fb46f9e06070a976fcb0b47e0c85c4eefff81f9
edgardfn <edgardfinotti@yahoo.com.br>
2021-02-28 14:33:36 -0300

    Estilizado a parte de informações do perfil exibida ao usuário.

--- quinzena8/projetoAstroMatch/projeto-astro-match/src/components/Perfil/Perfil.js
+++ quinzena8/projetoAstroMatch/projeto-astro-match/src/components/Perfil/Perfil.js
@@ -1,13 +1,22 @@
 import React from 'react'
-import { DivisaoCardPerfil } from './stylesPerfil'
+import { DivisaoCardPerfil, NomeIdade, DivisaoInformacoes, Bio } from './stylesPerfil'
 
 export function Perfil(props) {
     
-    console.log("perfil", props.perfil.photo)
+    console.log("perfil", props.perfil)
 
     return (
         <DivisaoCardPerfil imagem={props.perfil.photo}>
-           
+
+            <DivisaoInformacoes>
+                
+                <NomeIdade>{props.perfil.name}, {props.perfil.age} anos</NomeIdade>
+            
+                <Bio>{props.perfil.bio}</Bio>
+                
+            </DivisaoInformacoes>
+            
+
         </DivisaoCardPerfil>
     )
 }
--- quinzena8/projetoAstroMatch/projeto-astro-match/src/components/Perfil/stylesPerfil.js
+++ quinzena8/projetoAstroMatch/projeto-astro-match/src/components/Perfil/stylesPerfil.js
@@ -5,8 +5,39 @@ export const DivisaoCardPerfil = styled.div`
     background-repeat: no-repeat;
     background-size: cover;
     background-position: center;
-    border-radius: 4px;
-    box-shadow: 4px 4px #e0e0e0;
-    height: 80%;
+    border-radius: 8px;
+    box-shadow: 4px 4px 4px #e0e0e0;
+    height: 75%;
     margin: 16px 16px 0 16px;
+
+    display: flex;
+
+`
+
+export const DivisaoInformacoes = styled.div`
+    align-self: flex-end; 
+    width: 100%;
+
+    background-color: gray;
+    opacity: 0.6;
+    background-image: linear-gradient(gray, black);
+    border-radius: 8px;
+`
+
+
+export const NomeIdade = styled.h2`
+    color: white;
+    font-weight: bolder;
+    text-align: center;
+   
+`
+
+
+
+export const Bio = styled.h3`
+    color: white;
+    font-weight: bold;
+    text-align: center;
+    
+    
 `
