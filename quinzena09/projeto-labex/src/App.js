213c71e15b18a25a3ae92e9077b77e6c219c7062
edgardfn <edgardfinotti@yahoo.com.br>
2021-03-13 17:11:10 -0300

    Criado cabeçalho da estrutura.

--- quinzena09/projeto-labex/src/App.js
+++ quinzena09/projeto-labex/src/App.js
@@ -1,24 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import { DivHeader, DivLogo, DiviIconMenu, IconLogo, ImageProfileUser, TextLogo, DivIconProfileUser } from './styledApp'
+import ListIcon from '@material-ui/icons/List';
+import rocketIcon from './assets/rocket.png'
+import profileIcon from './assets/profile-user.png'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import { Tooltip } from '@material-ui/core';
 
 function App() {
+  const [activeMenu, setActiveMenu] = useState(false)
+
+  const openMenu = () => {
+    setActiveMenu(!activeMenu)
+  }
+
+  console.log("activeMenu", activeMenu)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <DivHeader>
+      <DiviIconMenu>
+        <Tooltip title="Menu" onClick={openMenu}> 
+          <ListIcon  
+            style={{ fontSize: 30 }}
+          />
+        </Tooltip>
+      </DiviIconMenu>
+      <DivLogo>
+        <IconLogo src={rocketIcon} />
+        <TextLogo>LabeX</TextLogo>
+      </DivLogo>
+      <DivIconProfileUser>
+        <Tooltip title="Login">
+          <AccountCircleIcon 
+            color="disabled"
+            style={{ fontSize: 40 }}
+          />
+        </Tooltip>
+      </DivIconProfileUser>
+    </DivHeader>
   );
 }
 
---
+++ quinzena09/projeto-labex/src/styledApp.js
@@ -0,0 +1,44 @@
+import styled  from 'styled-components'
+import ListIcon from '@material-ui/icons/List';
+
+export const DivHeader = styled.div`
+    width: 100%;
+    height: 90px;
+    border: 1px solid gray;
+
+    /* display: flex;
+    justify-content: space-around;
+    align-items: center; */
+    display: grid;
+    grid-template-columns: 1fr 1fr 1fr;
+`
+
+export const DiviIconMenu = styled.div`
+    height: 90px;
+    display: flex;
+    align-items: center;
+    margin-left: 40px;
+`
+
+export const DivLogo = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 90px;
+`
+
+export const DivIconProfileUser = styled.div`
+    height: 90px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`
+
+export const IconLogo = styled.img`
+    height: 50%;
+    margin: 0 8px;
+`
+
+export const TextLogo = styled.h1`
+    
+`
