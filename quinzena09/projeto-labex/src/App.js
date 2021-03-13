1b1c0ee0decb0c5c77080b4e99a02b00da9567a8
edgardfn <edgardfinotti@yahoo.com.br>
2021-03-13 17:24:34 -0300

    Criado novo componente Header e informações criadas anteriormente passadas para esse novo componente.

--- quinzena09/projeto-labex/src/App.js
+++ quinzena09/projeto-labex/src/App.js
@@ -1,10 +1,5 @@
 import React, { useState } from 'react'
-import { DivHeader, DivLogo, DiviIconMenu, IconLogo, ImageProfileUser, TextLogo, DivIconProfileUser } from './styledApp'
-import ListIcon from '@material-ui/icons/List';
-import rocketIcon from './assets/rocket.png'
-import profileIcon from './assets/profile-user.png'
-import AccountCircleIcon from '@material-ui/icons/AccountCircle';
-import { Tooltip } from '@material-ui/core';
+import Header from './components/Header/Header'
 
 function App() {
   const [activeMenu, setActiveMenu] = useState(false)
@@ -15,27 +10,9 @@ function App() {
 
   console.log("activeMenu", activeMenu)
   return (
-    <DivHeader>
-      <DiviIconMenu>
-        <Tooltip title="Menu" onClick={openMenu}> 
-          <ListIcon  
-            style={{ fontSize: 30 }}
-          />
-        </Tooltip>
-      </DiviIconMenu>
-      <DivLogo>
-        <IconLogo src={rocketIcon} />
-        <TextLogo>LabeX</TextLogo>
-      </DivLogo>
-      <DivIconProfileUser>
-        <Tooltip title="Login">
-          <AccountCircleIcon 
-            color="disabled"
-            style={{ fontSize: 40 }}
-          />
-        </Tooltip>
-      </DivIconProfileUser>
-    </DivHeader>
+    <Header 
+      onClickMenu={openMenu}
+    />
   );
 }
 
---
+++ quinzena09/projeto-labex/src/components/Header/Header.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import {DivHeader, DivIconProfileUser, DivLogo, DiviIconMenu, IconLogo, TextLogo} from './stylesHeader'
+import ListIcon from '@material-ui/icons/List'
+import rocketIcon from '../../assets/rocket.png'
+import AccountCircleIcon from '@material-ui/icons/AccountCircle'
+import { Tooltip } from '@material-ui/core'
+
+export default function Header ({onClickMenu}) {
+
+    return (
+        <DivHeader>
+      <DiviIconMenu>
+        <Tooltip title="Menu" onClick={onClickMenu} >  
+          <ListIcon  
+            style={{ fontSize: 30 }}
+          />
+        </Tooltip>
+      </DiviIconMenu>
+      <DivLogo>
+        <IconLogo src={rocketIcon} />
+        <TextLogo>LabeX</TextLogo>
+      </DivLogo>
+      <DivIconProfileUser>
+        <Tooltip title="Login">
+          <AccountCircleIcon 
+            color="disabled"
+            style={{ fontSize: 40 }}
+          />
+        </Tooltip>
+      </DivIconProfileUser>
+    </DivHeader>
+    )
+}
